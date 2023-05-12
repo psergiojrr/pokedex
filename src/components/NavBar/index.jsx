7899335a203b6a5d462dc0416438ed9179d810db
Paulo Sérgio <paulo.sergiodacosta@hotmail.com>
2023-05-12 14:08:31 -0300

    pokedex app

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function NavBar({pokemonFilter}) {
+  return (
+    <Box sx={{ flexGrow: 1, marginBottom: "2em"}}>
+      <AppBar position="static" sx={{background: "black"}}>
+        <Toolbar>
+          <Box display="flex" justifyContent="space-between" width="100%">
+            <Box component="img" src='./assets/pokemon-logo-black-transparent.png' sx={{maxWidth: 100}}/>
+            <Search onKeyUp={(event) => pokemonFilter(event.target.value)} sx={{maxHeight: 50, margin: 2}}>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Pesquisando..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/components/PokemonCard/index.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
+
+export default function PokemonCard({name, image, types, abilities}) {
+  const typeHandler = () => {
+    if(types[1]){
+      return types[0].type.name+" | "+types[1].type.name
+    }
+    return types[0].type.name
+  }
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" height="300" image={image} alt="Pokemon info"/>
+      <CardContent>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography >
+          <Typography gutterBottom variant="caption" component="div">
+            {typeHandler()}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
